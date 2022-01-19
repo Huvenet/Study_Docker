764cf825425a1a6c85d1bfff990a145c39d65f5a
times132 <times132@gmail.com>
2022-01-19 22:16:35 +0900

    spring boot docker test

---
+++ Jin/sample/backend/src/test/java/com/times/dockerExample/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.times.dockerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
